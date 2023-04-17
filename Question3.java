4ae6ab7e89ecad20a72447fc0dc866b2eda0ebb3
Rachelle <39646256+IceFire246@users.noreply.github.com>
2023-04-17 11:29:54 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
     
+    int result = num * num;
+    System.out.println(result);
   }
 }
